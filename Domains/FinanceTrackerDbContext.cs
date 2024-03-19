5148193dd622bc7641854f5da665d22bebc1541e
Alan Wang <alanw707@gmail.com>
2024-03-19 12:35:15 -0700

    refactor

---
+++ Domains/FinanceTrackerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using aa_finance_tracker.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace aa_finance_tracker.Data
+{
+    public class FinanceTrackerDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+
+        public DbSet<ExpenseCategory> ExpensesCategories { get; set; }
+
+        public FinanceTrackerDbContext(IConfiguration configuration)
+            : base(new DbContextOptionsBuilder<FinanceTrackerDbContext>()
+                .UseSqlServer(configuration.GetConnectionString("LocalDockerSQL"))
+                .Options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {                        
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<ExpenseType>()
+                .HasKey(e => e.Name);
+
+            modelBuilder.Entity<ExpenseCategory>()
+               .HasKey(e => e.Name);
+        }
+
+    }
+}
