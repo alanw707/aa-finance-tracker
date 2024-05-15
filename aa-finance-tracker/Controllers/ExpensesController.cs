a2a5bc290b1feb97ce41afa05fc1cd3baa195895
Alan Wang <alanw707@gmail.com>
2024-05-15 05:45:23 -0700

    WIP fix expense post action resolve conflict

--- aa-finance-tracker/Controllers/ExpensesController.cs
+++ aa-finance-tracker/Controllers/ExpensesController.cs
@@ -74,7 +74,7 @@ namespace AAFinanceTracker.API.Controllers
         // POST: api/Expenses
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<ExpenseModel>> PostExpense(ExpenseModel expenseModel, CancellationToken cancellationToken)
+        public async Task<ActionResult<ExpenseModel>> PostExpense(ExpenseModel expenseModel, [FromServices] IRepository<ExpenseType> expenseTypesRepo, [FromServices] IRepository<ExpenseCategory> expenseCategoryRepo, CancellationToken cancellationToken)
         {
 
             var expenseRepository = services.GetRequiredService<IRepository<Expense>>();
