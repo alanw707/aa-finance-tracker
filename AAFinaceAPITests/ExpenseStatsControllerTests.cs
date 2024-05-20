c78ed9ea16f98b690279e8e3a460fe13ec81e038
Alan Wang <alanw707@gmail.com>
2024-05-19 20:29:52 -0700

    Updated packages

--- AAFinaceAPITests/ExpenseStatsControllerTests.cs
+++ AAFinaceAPITests/ExpenseStatsControllerTests.cs
@@ -37,11 +37,9 @@ public class ExpenseStatsControllerTests
         // Act
         var result = await expenseStatsController.GetExpensesByCategoryYear(categoryName, year, CancellationToken.None);
 
-        // Assert
-        Assert.IsType<OkObjectResult>(result.Result);
+        // Assert        
         Assert.NotNull(result);
-        var okResult = result.Result as OkObjectResult;
-        Assert.NotNull(okResult);
+        var okResult = Assert.IsAssignableFrom<OkObjectResult>(result.Result);
         Assert.Equal(expenses, okResult.Value);
     }
 
@@ -53,12 +51,12 @@ public class ExpenseStatsControllerTests
         int year = 2023;
 
         expenseRepository.Setup(repo => repo.GetExpensesByCategoryYear(category, year, CancellationToken.None))
-             .ReturnsAsync(new List<Expense>());        
+             .ReturnsAsync(new List<Expense>());
 
         // Act
         var result = await expenseStatsController.GetExpensesByCategoryYear(category, year, CancellationToken.None);
 
         // Assert
         Assert.IsType<NotFoundResult>(result.Result);
-    }  
+    }
 }
